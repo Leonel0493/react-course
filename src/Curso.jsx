86c01ed4e0b81435563205f203e548d3781bdceb
Leonel Rivas <leonrivas04@gmail.com>
2021-07-02 20:28:54 -0600

    Update Curso.jsx
    Propiedades en JSX

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,24 +1,22 @@
 import React, { Fragment } from 'react'
 
-const mayorDeEdad = edad => edad >= 18
-const persona = { "nombre": "Leonel", "apellido": "Rivas", "edad": 19 }
+const curso = {
+    "title": "Curso Bases de Datos Desde Cero",
+    "description": "Diseña, estructura y administra bases de datos SQL y crea un sistema de facturación",
+    "image": "https://ed-grid.com/assets/img/poster-curso.png",
+    "price": "40Usd"
+}
 
 const Curso = () => (
     <article className="s-shadow-bottom">
 
         <div className="s-ratio-16-9 img-container s-radius-tl s-radius-tr">
-            <img src="https://ed-grid.com/assets/img/poster-curso.png" alt="Curso Imagen" />
+            <img src={curso.image} alt={curso.title} />
         </div>
 
         <div className="s-bg-white s-pxy-2">
-            <h3>{`Hola, me llamo ${persona.nombre} y tengo ${persona.edad}`}</h3>
-            {
-                mayorDeEdad(persona.edad) 
-                ?
-                    <p className="s-mb-0">Soy mayor de edad</p>
-                :
-                    <p className="s-mb-0">Soy menor de edad</p>
-            }
+            <h3>{curso.title}</h3>
+            <p className="s-mb-0">{curso.description}</p>
         </div>
         <footer className="s-cross-center s-bg-grey s-pxy-2 s-radius-br s-radius-bl">
 
@@ -29,9 +27,9 @@ const Curso = () => (
                 </div>
 
             </div>
-            <p className="s-mb-0">{`Prof. ${persona.nombre} ${persona.apellido}`}</p>
+            <p className="s-mb-0">Prof. Alexys Lozada</p>
 
-            <div className="button s-to-right">$40USD</div>
+            <div className="button s-to-right">{`$ ${curso.price}`}</div>
         </footer>
     </article>
 )
