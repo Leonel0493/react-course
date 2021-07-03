32a9faee8ea6b83313c9f2d14c4660a01097ff74
Leonel Rivas <leonrivas04@gmail.com>
2021-07-02 21:12:38 -0600

    Update App and Curso
    Imprimiendo propiedades

--- src/App.js
+++ src/App.js
@@ -18,7 +18,30 @@ const App = () => (
     </div>
 
     <div className="ed-grid m-grid-3">
-      <Curso />
+      <Curso 
+        title="Curso de Rasengan" 
+        image="https://laverdadnoticias.com/__export/1606513512175/sites/laverdad/img/2020/11/27/naruto_5_jutsu_que_encajarian_mejor_con_el_heroe_de_konoha_que_rrasengan.jpg_1183029055.jpg" 
+        description="Poderoso Ninjutsu de Rango A creado originalmente por el Cuarto Hokage, Minato Namikaze"
+        price="40Usd"
+        profesor="Kanohamaru"
+        prof_image="https://laverdadnoticias.com/__export/1615568724533/sites/laverdad/img/2021/03/12/konohamaru_sarutobi_boruto_anime.jpg_514966513.jpg"
+      />
+      <Curso 
+        title="Taijutsu" 
+        image="http://pm1.narvii.com/6261/35e120a41b3d026c939a67e13327ec30619b1892_00.jpg" 
+        description="El Taijutsu son todos aquellos jutsus que tienen que ver con el combate mano a mano"
+        price="45Usd"
+        profesor="Rock Lee"
+        prof_image="http://images6.fanpop.com/image/photos/34800000/rock-lee-rock-lee-fanfic-pictures-34826515-600-450.jpg"
+      />
+      <Curso 
+        title="Dojutsu" 
+        image="https://www.cultture.com/pics/2020/11/naruto-en-que-dojutsu-te-basarias-para-tu-signo-del-zodiaco-0.jpg" 
+        description="Un Dojutsu es un tipo de técnica ninja que existe en el universo ficticio del manga y anime Naruto"
+        price="35Usd"
+        profesor="Hyuga Hanabi"
+        prof_image="https://pm1.narvii.com/6801/79fe99d902ca0f453650913c85ff0834e1b8fa41v2_hq.jpg"
+      />
     </div>
   </>
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,35 +1,28 @@
 import React, { Fragment } from 'react'
 
-const curso = {
-    "title": "Curso Bases de Datos Desde Cero",
-    "description": "Diseña, estructura y administra bases de datos SQL y crea un sistema de facturación",
-    "image": "https://ed-grid.com/assets/img/poster-curso.png",
-    "price": "40Usd"
-}
-
-const Curso = () => (
-    <article className="s-shadow-bottom">
+const Curso = ({title, image, description, price, profesor, prof_image}) => (
+    <article className="s-shadow-bottom" id={title}>
 
         <div className="s-ratio-16-9 img-container s-radius-tl s-radius-tr">
-            <img src={curso.image} alt={curso.title} />
+            <img src={image} alt={title} />
         </div>
 
         <div className="s-bg-white s-pxy-2">
-            <h3>{curso.title}</h3>
-            <p className="s-mb-0">{curso.description}</p>
+            <h3>{title}</h3>
+            <p className="s-mb-0">{description}</p>
         </div>
         <footer className="s-cross-center s-bg-grey s-pxy-2 s-radius-br s-radius-bl">
 
             <div className="s-10 s-mr-1">
 
                 <div className="circle ">
-                    <img src="https://ed-grid.com/assets/img/alexys.jpg" alt="foto del profesor" />
+                    <img src={prof_image} alt="foto del profesor" />
                 </div>
 
             </div>
-            <p className="s-mb-0">Prof. Alexys Lozada</p>
+            <p className="s-mb-0">{`Prof. ${profesor}`}</p>
 
-            <div className="button s-to-right">{`$ ${curso.price}`}</div>
+            <div className="button s-to-right">{`$ ${price}`}</div>
         </footer>
     </article>
 )
