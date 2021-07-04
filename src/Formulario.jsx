404210d2153b029daeca930a69b88bc8b1883227
Leonel Rivas <leonrivas04@gmail.com>
2021-07-04 10:52:48 -0600

    Estados
    Actualizando estados del componente

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,27 +1,48 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 
 class Formulario extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
+
+        this.state = {
+            nombre: "",
+            correo: ""
+        }
     }
 
-    render(){
-        return(
+    //  this.setState({})
+
+    render() {
+        return (
             <div className="ed-grid">
                 <h1>Formulario</h1>
                 <form>
-                    <div className="form__item">
-                    <label>Nombre Completo</label>
-                        <input type="text" />
-                    </div>
-                    <div className="form__item">
-                        <label>Correo Electronico</label>
-                        <input type="email" />
-                    </div>
-                    <div className="form__item">
-                        <input className="button full" type="submit" value="Enviar" />
+                    <div className="ed-grid m-grid-2">
+                        <div className="form__item">
+                            <label>Nombre Completo</label>
+                            <input 
+                                type="text" 
+                                onChange={(e) => this.setState({
+                                    nombre: e.target.value
+                                })} 
+                            />
+                        </div>
+                        <div className="form__item">
+                            <label>Correo Electronico</label>
+                            <input 
+                                type="email"
+                                onChange={(e) => this.setState({
+                                    correo: e.target.value
+                                })}
+                            />
+                        </div>
                     </div>
                 </form>
+
+                <div>
+                    <h2>{`Hola ${this.state.nombre}`}</h2>
+                    <span>{`Tu correo es: ${this.state.correo}`}</span>
+                </div>
             </div>
         )
     }
